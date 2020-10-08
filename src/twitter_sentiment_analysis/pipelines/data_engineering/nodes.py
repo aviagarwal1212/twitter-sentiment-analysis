dc866f1d7403dcac842b23c1a115450aaed9cb7a
Avi Agarwal <avidiotic@users.noreply.github.com>
2020-10-08 16:30:28 +0530

    add doctstrings for nodes.py for each pipeline

--- src/twitter_sentiment_analysis/pipelines/data_engineering/nodes.py
+++ src/twitter_sentiment_analysis/pipelines/data_engineering/nodes.py
@@ -14,14 +14,32 @@ nltk.download('wordnet')
 # Primary Functions
 
 def clean_raw_data(data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Select pertinent columns from the full dataset.
     
+     Args:
+        data: Source data.
+        
+     Returns:
+        Cleaned data.
+        
+    '''
     _pertinent_columns = ['sentiment', 'text']
     data = data[_pertinent_columns]
     
     return data
     
 def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Preprocess pertinent data.
     
+     Args:
+        data: Cleaned data.
+        
+     Returns:
+        Processed data.
+        
+    '''
     data['text'] = data['text'].apply(_convert_links)
     data['text'] = data['text'].apply(_convert_usernames)
     data['text'] = data['text'].apply(_convert_emoticons)
@@ -33,23 +51,19 @@ def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
 # Seconday Functions
 
 def _convert_emoticons(text: str) -> str:
-    
     for emot in EMOTICONS:
         text = re.sub(emot, EMOTICONS[emot], text)
     return text
     
 def _convert_links(text: str) -> str:
-
     text = re.sub(r'(https?://\S+)|(www.\S+)', ' link ', text, flags = re.IGNORECASE)
     return text
     
 def _convert_usernames(text: str) -> str:
-    
     text = re.sub(r'@\S+', ' username ', text)
     return text
     
 def _lemmatize(text: str) -> str:
-    
     stop_words = stopwords.words('english')
     lemmatizer = WordNetLemmatizer()
     text = text.lower()
--- src/twitter_sentiment_analysis/pipelines/data_science/nodes.py
+++ src/twitter_sentiment_analysis/pipelines/data_science/nodes.py
@@ -13,7 +13,17 @@ from sklearn.metrics import classification_report
 # Primary Functions
 
 def split_data(data: pd.DataFrame, parameters: Dict) -> List:
+    '''
+    Splits data into training and test set.
     
+     Args:
+        data: Source processed data.
+        parameters: Parameters defined in parameter.yml.
+    
+     Returns:
+        A list containing split data.
+        
+    '''
     data = _handle_empty(data)
     X = data['text'].values
     y = data['sentiment'].values
@@ -21,7 +31,18 @@ def split_data(data: pd.DataFrame, parameters: Dict) -> List:
     return [X_train, X_test, y_train, y_test]
     
 def vectorize_text(X_train: np.ndarray, X_test: np.ndarray, parameters: Dict) -> List:
+    '''
+    Vectorize text column in train and test set.
     
+     Args:
+        X_train: Training text data.
+        X_test: Testing text data.
+        parameters: Parameters defined in parameter.yml.   
+       
+     Returns:
+        A list containing vectorized train and test feature sets. 
+        
+    '''
     vectorizer = TfidfVectorizer(ngram_range=(parameters['ngram_range_min'], parameters['ngram_range_max']), max_features=parameters['max_features'])
     vectorizer.fit(X_train)
     X_train = vectorizer.transform(X_train)
@@ -29,13 +50,32 @@ def vectorize_text(X_train: np.ndarray, X_test: np.ndarray, parameters: Dict) ->
     return [X_train, X_test]
     
 def train_model(X_train: np.ndarray, y_train: np.ndarray, parameters: Dict) -> LogisticRegression:
+    '''
+    Train the logistic regression model.
     
+     Args:
+        X_train: Vectorized training text data.
+        y_train: Training data for sentiment.
+        parameters: Parameters defined in parameter.yml.
+        
+     Returns:
+        Trained model.
+        
+    '''
     model = LogisticRegression(max_iter=parameters['max_iter'], n_jobs=-1, C=0.5)
     model.fit(X_train, y_train)
     return model
     
 def evaluate_model(X_test: np.ndarray, y_test: np.ndarray, model: LogisticRegression):
+    '''
+    Generate and log classification report for test data.
     
+     Args:
+        X_test: Vectorized test text data.
+        y_test: Test data for sentiment.
+        model: Trained model.
+        
+    '''
     y_pred = model.predict(X_test)
     logger = logging.getLogger(__name__)
     logger.info(classification_report(y_test, y_pred))
